4bb6481162c77650fa844d34e4c2cd2f8a16183c
Andrew Whitehouse <andrew@whitehouse.org.uk>
2021-06-17 19:30:30 +0100

    Fix up for Heroku.

--- extreme_startup.js
+++ extreme_startup.js
@@ -18,6 +18,10 @@ app.get("/", function(req, res) {
     res.end(a);
 });
 
-app.listen(1337, "0.0.0.0");
-console.log("Server running on http://0.0.0.0:1337/");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port, "0.0.0.0");
+console.log(`Server running on http://0.0.0.0:${port}/`);
 
